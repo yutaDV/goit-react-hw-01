2fbb18df0d5c3fa12bc415ae751601083cbb23af
Yuliia <74568169+yutaDV@users.noreply.github.com>
2024-07-28 18:51:54 +0300

    task 3 draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import userData from './userData.json';
-import friends from './friends.json';
+import userData from './data/userData.json';
+import friends from './data/friends.json';
+import items from './data/transactions.json';
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
-
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 const App = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={items} />
     </>
   );
 };
